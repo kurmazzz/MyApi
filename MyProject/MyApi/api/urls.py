584186606d03f650dfc03b959b35607aa1ae6cc8
kurmazzz <46731016+kurmazzz@users.noreply.github.com>
2021-01-19 00:51:05 +0300

    Update urls.py
    Добавление нового урла

--- MyProject/MyApi/api/urls.py
+++ MyProject/MyApi/api/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 from django.urls import include
 from .views import firstFunction
 from rest_framework.routers import DefaultRouter
-from .views import ProjectsViewSet
+from .views import ProjectsViewSet, EventsViewSet
 
 router = DefaultRouter()
 router.register('projects', ProjectsViewSet, basename='projects')
+router.register('events', EventsViewSet, basename='events')
 
 urlpatterns = [
     url('first', firstFunction),
